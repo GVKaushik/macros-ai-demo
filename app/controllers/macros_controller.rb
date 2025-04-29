9bc7c844a7c6c1efe294877a07f6c3a3cb5dc072
GVKaushik <gvkaushik11@gmail.com>
2025-04-29 23:39:35 +0000

    built form and prelim output

---
+++ app/controllers/macros_controller.rb
@@ -0,0 +1,9 @@
+class MacrosController < ApplicationController
+  def display_form
+    render({:template=>"macro_templates/new_form"})
+  end
+
+  def do_magic
+    render({:template=>"macro_templates/process_inputs"})
+  end
+end
