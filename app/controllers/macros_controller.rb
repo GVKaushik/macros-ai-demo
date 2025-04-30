0793cea34cba3cc027ddb67015041e2f65ba19b6
GVKaushik <gvkaushik11@gmail.com>
2025-04-30 00:05:35 +0000

    openai being used

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -7,6 +7,13 @@ class MacrosController < ApplicationController
     @desc = params.fetch("desc_param")
     @image = params.fetch("image_param")
     @image_conv = DataURI.convert(@image)
+    c = OpenAI::Chat.new
+    c.system("You are an expert nutritionist. Estimate the macronutrients (carbohydrates, protein and fat) in grams, as well as in total calories in kcal.")
+    c.user(@desc,image:@image)
+    @result = c.assistant!
+
+
+
     render({:template=>"macro_templates/process_inputs"})
   end
 end
